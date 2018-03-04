84b0de3d6bdc826defcc77b7dbd3441d4ed813eb
Becky Afzali <beckyafzali@gmail.com>
2018-03-04 09:20:29 -0600

    setting up start game function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,65 +1,87 @@
-// Define the array of words to choose from
+// Define global variables needed for game
+let randomWord;
+let answerString;
+let lives = 10;
+let wins = 0;
+let letter;
+let keyPressedString;
+let gameOver = false;
+let index;
+
+// Define the arrays
 let words =["maple", "canoe", "wolf", "lake", "forest"];
 let wordArray = [];
+let answer = [];
+let keysPressed = [];
 
-// Select the random words from words array
-let randomWord = words[Math.floor(Math.random() * words.length)];
-
+// Select the random words from words array and then split the selected word into an array of it's letters
+randomWord = words[Math.floor(Math.random() * words.length)];
 wordArray = randomWord.split("");
-
 //To test split of random word REMOVE WHEN FINISHED
 console.log(wordArray);
 
-// Define global variables needed for game
-var answerString;
-let lives = 10;
-let wins = 0;
-var answer = [];
-var keyPressed = [];
-var letter;
+// To select random word from the  and display blank lines for each letter in the word
+for (var i = 0; i < wordArray.length; i++) {
+  answer[i] = ("_ ");
+}
 
-    // To select random word from the  and display blank lines for each letter in the word
-    for (var i = 0; i < wordArray.length; i++) {
-      answer[i] = ("_ ");
-    }
-  
-    // To turn the answer array into a string
-    answerString = answer.join('');
-  
-    // To display the answer string on the page
-    document.querySelector("#randomWord").innerHTML = answerString;
+// Create a function to create a string from the answer array
+let displayAnswerString = function() {
+  // To turn the answer array into a string
+  answerString = answer.join('');
+  // To display the answer string on the page
+  document.querySelector("#randomWord").innerHTML = answerString;
+};
 
-    //To display the total lives:
-    document.querySelector("#lives").innerHTML = lives;
-       
-    // define what happens when a key is pressed
-    while (lives > 0) {
+displayAnswerString();
+
+document.querySelector("#lives").innerHTML = lives;
+
+
+let startGame = function() {
+  // define what happens when a key is pressed          
+  document.onkeyup = function(event) {
+    
+    letter = String.fromCharCode(event.which).toLowerCase();
+    
+    keysPressed.push(letter);
+    
+    if (wordArray.indexOf(letter) !== -1) {
       
-      document.onkeyup = function(event) {
-
-        letter = String.fromCharCode(event.which).toLowerCase();
-
-        keyPressed.push(letter);
-
-        if (wordArray.indexOf(letter) !== -1) {
-
-          let index = wordArray.indexOf(letter);
-          answer.splice(index, 1, letter);
-          answerString = answer.join('');
-          document.querySelector("#randomWord").innerHTML = answerString; 
-
-          // return answer;
+      index = wordArray.indexOf(letter);
+      answer.splice(index, 1, letter);
+      console.log(index);
+      displayAnswerString();
+      
+    } else {
+      keyPressedString = keysPressed.join(' ');
+      document.querySelector("#lettersGuessed").innerHTML = keyPressedString;
+      lives = lives - 1;
+      document.querySelector("#lives").innerHTML = lives;
+    }
     
-        } else {
+    if (lives === 0) {
+      gameOver = true;
+      document.querySelector("#gameOver").innerHtml = "Sorry, you lose!";
+    }
     
-          keyPressedString = keyPressed.join(' ');
-          document.querySelector("#lettersGuessed").innerHTML = keyPressedString;
-          document.querySelector("#lives").innerHTML = lives - 1;
-    
-        }
+    if (answerString === randomWord) {
+      gameOver = true;
+    }
+    // return gameOver;
+  }; 
+  
+};
 
-      // return letter;
-      }
-    } 
+if (gameOver === false) {
+  startGame();
 
-    // let letterArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+} else if (gameOver === true && answerString === randomWord) {
+  console.log("You win");
+  wins = wins + 1;
+} else {
+  console.log("You lost");
+  
+}
+
+
