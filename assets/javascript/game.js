621f2b9b2e437e3667453c0f1ce987fa18b8e0d0
Becky Afzali <beckyafzali@gmail.com>
2018-03-03 18:18:25 -0600

    keyup function, letters on screen

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,41 +1,65 @@
-// Define the array of words to choose from and the random word variable
-let words =["moose", "maple tree", "canoe paddle", "wolf pack", ""];
-
-// Define the letters of the alphabet
+// Define the array of words to choose from
+let words =["maple", "canoe", "wolf", "lake", "forest"];
 let wordArray = [];
 
-let letterArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+// Select the random words from words array
 let randomWord = words[Math.floor(Math.random() * words.length)];
 
 wordArray = randomWord.split("");
+
+//To test split of random word REMOVE WHEN FINISHED
 console.log(wordArray);
 
+// Define global variables needed for game
 var answerString;
-var lives;
-var counter = 0;
+let lives = 10;
+let wins = 0;
 var answer = [];
 var keyPressed = [];
+var letter;
 
-// function to display blanks for each word
-// function begin() {
-
-  for (var i = 0; i < wordArray.length; i++) {
-
-    answer[i] = ("_");
-    answerString = answer.join('');
-    document.getElementById('#currentWord').innerHTML = answerString;
-
-  }
+    // To select random word from the  and display blank lines for each letter in the word
+    for (var i = 0; i < wordArray.length; i++) {
+      answer[i] = ("_ ");
+    }
   
+    // To turn the answer array into a string
+    answerString = answer.join('');
+  
+    // To display the answer string on the page
+    document.querySelector("#randomWord").innerHTML = answerString;
 
-// }
+    //To display the total lives:
+    document.querySelector("#lives").innerHTML = lives;
+       
+    // define what happens when a key is pressed
+    while (lives > 0) {
+      
+      document.onkeyup = function(event) {
 
-document.onkeyup = function(event) {
-  if (keyPressed === letterArray[] && keyPressed === wordArray[]) {
+        letter = String.fromCharCode(event.which).toLowerCase();
+
+        keyPressed.push(letter);
+
+        if (wordArray.indexOf(letter) !== -1) {
+
+          let index = wordArray.indexOf(letter);
+          answer.splice(index, 1, letter);
+          answerString = answer.join('');
+          document.querySelector("#randomWord").innerHTML = answerString; 
+
+          // return answer;
     
-  }
-}
+        } else {
+    
+          keyPressedString = keyPressed.join(' ');
+          document.querySelector("#lettersGuessed").innerHTML = keyPressedString;
+          document.querySelector("#lives").innerHTML = lives - 1;
+    
+        }
 
-// for (j = 0, j < wordArray, j++) {
-//   console.log("j");
-// }
+      // return letter;
+      }
+    } 
+
+    // let letterArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
